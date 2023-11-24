d084b9c97c010c7870087e1f91df8629f66a7dc2
Владислав Харченко <vladislav.harchenko2017@yandex.ru>
2023-11-25 00:02:26 +0700

    the posts section has been added

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,14 +13,17 @@ const mix = require('laravel-mix');
 
 mix.styles([
     'resources/assets/admin/plugins/fontawesome-free/css/all.min.css',
-    'resources/assets/admin/css/adminlte.min.css'
+    'resources/assets/admin/css/adminlte.min.css',
+    'resources/assets/admin/plugins/select2/dist/css/select2.css',
+    'resources/assets/admin/plugins/select2-bootstrap4-theme/dist/select2-bootstrap4.css',
 ], 'public/assets/admin/css/admin.css');
 
 mix.scripts([
     'resources/assets/admin/plugins/jquery/jquery.min.js',
     'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js',
     'resources/assets/admin/js/adminlte.min.js',
-    'resources/assets/admin/js/demo.js'
+    'resources/assets/admin/js/demo.js',
+    'resources/assets/admin/plugins/select2/dist/js/select2.full.js'
 ],'public/assets/admin/js/admin.js');
 
 mix.copyDirectory('resources/assets/admin/plugins/fontawesome-free/webfonts', 'public/assets/admin/webfonts');
